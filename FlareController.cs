063acbdc82da4accd928866fc53659fc2a81b082
Ximo Neem <mih.5.suh70@gmail.com>
2022-01-26 11:03:10 +0300

    1.0

---
+++ FlareController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlareController : MonoBehaviour
+{
+    private LensFlare flare;
+    private Camera mCamera;
+    [Header("Flare brightness")]
+    [Range(0, 1)]
+    public float multiplier = 1;
+    public float ratio = 700;
+    [Space]
+    public float minFlare = 1;
+    public float maxFlare = 150;
+
+    private void Start()
+    {
+        if (this.GetComponent<LensFlare>())
+        {
+            flare = this.GetComponent<LensFlare>();
+        }
+        else
+        {
+            flare = this.gameObject.AddComponent<LensFlare>();
+        }
+        mCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        if (flare != null)
+        {
+            flare.brightness = Mathf.Clamp(ratio / Vector3.Distance(this.transform.position, mCamera.transform.position) * multiplier, minFlare, maxFlare);
+        }
+    }
+}
